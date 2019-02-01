a1eaddad5c932de54f6794b111eafe8dade359ec
f1r3starter <andrey.filenochko@gmail.com>
2019-02-01 23:00:47 +0200

    added useranswer entity

---
+++ src/Entity/UserAnswer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Entity;
+
+class UserAnswer
+{
+    private $question;
+
+    private $answer;
+
+    private $user;
+
+    public function getQuestion(): ?Question
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(?Question $question): self
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    public function getAnswer(): ?Answer
+    {
+        return $this->answer;
+    }
+
+    public function setAnswer(?Answer $answer): self
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
---
+++ src/Manager/TestManager.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andreyfilenko
+ * Date: 2019-01-31
+ * Time: 22:33
+ */
+
+namespace App\Manager;
+
+
+use App\Entity\Test;
+use App\Entity\User;
+
+class TestManager
+{
+    public function getUserTestAnswers(User $user, Test $test)
+    {
+
+    }
+}
