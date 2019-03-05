9a554b524829e81a3058c4965783c091fb4d8188
f1r3starter <andrey.filenochko@gmail.com>
2019-03-05 23:09:38 +0200

    test controller

---
+++ src/Controller/TestController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TestRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class TestController extends AbstractController
+{
+    /**
+     * @var TestRepository
+     */
+    private $testRepository;
+
+    public function __construct(TestRepository $testRepository)
+    {
+
+        $this->testRepository = $testRepository;
+    }
+
+    public function getUserTests()
+    {
+        
+    }
+}
