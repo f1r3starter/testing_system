430d881ae4b2196516ad48bb69c316947f6ab603
f1r3starter <andrey.filenochko@gmail.com>
2019-05-24 22:26:34 +0300

    test entity

--- src/Domain/Test/Model/Test.php
+++ src/Domain/Test/Model/Test.php
@@ -9,4 +9,22 @@ class Test
     private $description;
 
     private $questions = [];
+
+    /**
+     * @param string $name
+     */
+    public function rename(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function changeDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function addQuestion(Question $question): void
+    {
+        $this->questions[] = $question;
+    }
 }
