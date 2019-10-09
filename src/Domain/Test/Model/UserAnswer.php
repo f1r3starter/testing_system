a819ca9936eae7f131b437831def769336784a4e
f1r3starter <andrey.filenochko@gmail.com>
2019-10-09 22:33:26 +0300

    updated entities

--- src/Domain/Test/Model/UserAnswer.php
+++ src/Domain/Test/Model/UserAnswer.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace App\Domain\Test\Model;
-
 
 class UserAnswer
 {
@@ -13,4 +11,36 @@ class UserAnswer
     private $seenQuestionAt;
 
     private $answeredAt;
+
+    /**
+     * @return mixed
+     */
+    public function getAnswer()
+    {
+        return $this->answer;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSeenQuestionAt()
+    {
+        return $this->seenQuestionAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAnsweredAt()
+    {
+        return $this->answeredAt;
+    }
 }
